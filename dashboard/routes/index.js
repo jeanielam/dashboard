46e66874afdd1b3577dc77e850ddeb87140f64f4
Jeanie Lam <jeanielam321@gmail.com>
2016-08-07 00:19:48 -0700

    horizontal scrollbar for transit results, auto refresh and sorting of next bus results

--- dashboard/routes/index.js
+++ dashboard/routes/index.js
@@ -12,7 +12,7 @@ exports.index = function(req, res) {
 };
 
 exports.transit = function(req, res) {
-	var stops = [52866, 52856, 53120, 53210, 53211];
+	var stops = [52866, 52856, 53120, 53210, 53211, 59316];
 	res.render('transit', {title: 'Transit', stops: stops});
 };
 
